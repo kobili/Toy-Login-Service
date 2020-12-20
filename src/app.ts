4b86b79f279b126ca1e0a8023109b53e766a44c0
kobili <likobe23@gmail.com>
2020-12-19 18:55:32 -0800

    Implemented skeleton login endpoint

--- src/app.ts
+++ src/app.ts
@@ -27,7 +27,27 @@ app.post("/api/v1/user/register", (req: Request, res: Response) => {
 });
 
 /**
- * 
+ * Request body needs an email and password
+ * Determines if email and password combo matches an entry in the database of users
+ * TODO: add jwt signing on successful login
  */
+app.post("/api/v1/user/login", (req: Request, res: Response) => {
+    let email: string = req.body.email;
+    let password: string = req.body.password;
+
+    let user: any = findUser(email);     // the user with the corresponding email
+
+    if (!user) {
+        return res.status(404).send({err: "User not found"});
+    }
+
+    if (user.password === password) {
+        console.log("Authentication successful");
+        return res.status(501).send({err: "Authentication successful; Signing not yet implemented"});
+    } else {
+        return res.status(401).send({err: `Incorrect password for user: ${email}`});
+    }
+});
+
 
 app.listen(5000, () => console.log("Server running @ http://localhost:5000"));
