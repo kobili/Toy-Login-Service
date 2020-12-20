abc9c161d72e72ec9c633ac84c624a44a69017a2
kobili <likobe23@gmail.com>
2020-12-19 19:29:00 -0800

    Added password encryption

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import express, { Application, Request, Response, NextFunction } from 'express';
 import bodyParser from 'body-parser';
+import bcrypt from 'bcrypt';
+
 import {addNewUser, findUser} from './User';
 import {connectDB} from './db'
 
@@ -18,19 +20,27 @@ connectDB();
 app.post("/api/v1/user/register", (req: Request, res: Response) => {
 
     let email: string = req.body.email;
-    let password: string = req.body.password
-
-    if (addNewUser(email, password) === null) {
+    if (findUser(email)) {
         return res.status(409).send({error: "Error: User already exists"});
-    } else {
-        return res.status(200).send({"email": email, "password": password});
     }
+
+    let password: string = req.body.password;
+
+    // hash the concatenation of the user's email and password
+    bcrypt.hash(email + password, 10, (err, hash) => {
+        if (err) {
+            return res.status(500).send({err: "Problem registering user"});
+        }
+
+        addNewUser(email, hash);
+        return res.status(200).send({"email": email, "password": hash});
+    })
+
 });
 
 /**
  * Request body needs an email and password
  * Determines if email and password combo matches an entry in the database of users
- * TODO: add jwt signing on successful login
  */
 app.post("/api/v1/user/login", (req: Request, res: Response) => {
     let email: string = req.body.email;
@@ -39,15 +49,23 @@ app.post("/api/v1/user/login", (req: Request, res: Response) => {
     let user: any = findUser(email);     // the user with the corresponding email
 
     if (!user) {
-        return res.status(404).send({err: "User not found"});
+        return res.status(404).send({err: `No user found with email ${email}`});
     }
 
-    if (user.password === password) {
-        console.log("Authentication successful");
-        return res.status(501).send({err: "Authentication successful; Signing not yet implemented"});
-    } else {
-        return res.status(401).send({err: `Incorrect password for user: ${email}`});
-    }
+    // determine if the email and password are correct
+    bcrypt.compare(email + password, user.password, (err, result) => {
+        if (err) {
+            return res.status(500).send({err: "There was a problem with logging you in :("});
+        }
+
+        if (result) {
+            // TODO: add jwt signing on successful login
+            console.log("Authentication successful");
+            return res.status(501).send({err: "Authentication successful; Signing not yet implemented"});
+        } else {
+            return res.status(401).send({err: `Incorrect password for user: ${email}`});
+        }
+    });
 });
 
 
